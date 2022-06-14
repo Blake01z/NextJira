28644f9de465900464e4b5ea797ae7a3d9fd2dc7
Daniel Correa <daniel.correa92@hotmail.com>
2022-06-13 21:14:04 -0500

    Configuramos los themes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next'
 
 
 const HomePage: NextPage = () => {
   return (
-   <h1>Hola Mundo</h1>
+    <Typography variant='h1' color='primary'>Hola Mundo</Typography>
   )
 }
 
