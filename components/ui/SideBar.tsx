872d5725945f52986eacff83132ba53f7f583c56
Daniel Correa <daniel.correa92@hotmail.com>
2022-06-13 22:02:38 -0500

    Agregamos los componentes del alyout

---
+++ components/ui/SideBar.tsx
@@ -0,0 +1,54 @@
+import { Drawer, Box, Typography, List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+
+const menuItems: string[] = ['Inbox','Starred', 'Send Email', 'Draft']
+
+const SideBar = () => {
+  return (
+    <Drawer
+        anchor="left"
+        open={true}
+        onClose={() => console.log('cerrando')}
+    >   
+
+        <Box sx={{width:250}}>
+
+            <Box sx={{padding: '5px 10px'}}>
+                <Typography variant="h4">Menú</Typography>
+            </Box>
+
+            <List>
+                {
+                    menuItems.map((text,index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 ? <InboxOutlinedIcon/> : <EmailOutlinedIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}></ListItemText>
+                        </ListItem>
+                    ))
+                }
+            </List>
+
+            <Divider />
+
+            <List>
+                {
+                    menuItems.map((text,index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 ? <InboxOutlinedIcon/> : <EmailOutlinedIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}></ListItemText>
+                        </ListItem>
+                    ))
+                }
+            </List>
+
+        </Box>
+    </Drawer>
+  )
+}
+
+export { SideBar }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import { Typography } from '@mui/material';
 import type { NextPage } from 'next'
+import { Layout } from '../components/layouts';
 
 
 const HomePage: NextPage = () => {
   return (
-    <Typography variant='h1' color='primary'>Hola Mundo</Typography>
+    <Layout title="Home Page">
+      <Typography variant='h1' color='primary'>Hola Mundo</Typography>
+    </Layout>
   )
 }
 
