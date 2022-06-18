362aa18ed956dd70703935a264fbf9a028c1f6ae
Daniel Correa <daniel.correa92@hotmail.com>
2022-06-18 14:32:02 -0500

    Agregamos los snipets

--- context/ui/UIProvider.tsx
+++ context/ui/UIProvider.tsx
@@ -1,29 +1,28 @@
 import { FC, useReducer } from 'react';
 import { UIContext, uiReducer } from './';
 
+
 export interface UIState {
-    sidemenuOpen: boolean
+   sidemenuOpen: boolean
 }
+
 
 const UI_INITIAL_STATE: UIState = {
     sidemenuOpen: false
 }
 
+
 export const UIProvider:FC = ({children}) => {
 
     const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE);
 
-  return (
-    <UIContext.Provider
-        value={{
-            sidemenuOpen: false
-        }}
-    >
-      {children}
-    </UIContext.Provider>
-  )
+    return (
+         <UIContext.Provider
+                value={{
+                    sidemenuOpen: false
+                }}
+                 >
+                  {children}
+         </UIContext.Provider>
+     )
 }
-
-
-
-
