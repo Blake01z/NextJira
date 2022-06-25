0bc62c87b1ecf63f19d24d1f896473e8b6f0d0f2
Daniel Correa <daniel.correa92@hotmail.com>
2022-06-25 15:16:49 -0500

    Agregamos la funcion para crear entradas desde el front

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,5 +1,4 @@
 import { FC, useEffect, useReducer } from 'react';
-import {v4 as uuidv4} from 'uuid';
 
 import { EntriesContext, entriesReducer } from './';
 import { Entry } from '../../interfaces';
@@ -19,16 +18,15 @@ export const EntriesProvider:FC = ({children}) => {
 
     const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
-    const addNewEntry = (description:string) => {
-        
-        const newEntry: Entry = {
-            _id: uuidv4(),
-            description,
-            createdAt: Date.now(),
-            status: 'pending'
+    const addNewEntry = async (description:string) => {
+
+        try {
+            const {data} = await entriesApi.post<Entry>('/entries',{description});
+            dispatch({type: '[Entry] Add-Entry', payload: data})
+        } catch (error) {
+            console.log(error)
         }
 
-        dispatch({type: '[Entry] Add-Entry', payload: newEntry})
 
     }
 
