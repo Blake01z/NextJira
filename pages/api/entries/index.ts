58041cf0468d02cf129f1e9254cfa305eda8dd35
Daniel Correa <daniel.correa92@hotmail.com>
2022-06-25 15:02:31 -0500

    Agregamos la funcion para crear una entrada

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -6,6 +6,7 @@ import { Entry, IEntry } from '../../../models';
 type Data = 
     | { message: string }
     | IEntry[]
+    | IEntry
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
@@ -13,6 +14,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
     switch(req.method){
         case 'GET':
             return getEntries(res)
+
+        case 'POST':
+            return postEntry(req,res);
 
         default:
             return res.status(400).json({ message: 'Endpoint no existe' });
@@ -28,3 +32,28 @@ const getEntries = async (res:NextApiResponse<Data>) => {
 
     res.status(200).json(entries)
 }
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const {description = ''} = req.body;
+
+    const newEntry = new Entry({
+        description,
+        createdAt: Date.now(),
+    })
+
+    try {
+        await db.connect();
+        await newEntry.save();
+        await db.disconnect();
+
+        return res.status(201).json(newEntry);
+
+    } catch (error) {
+        await db.disconnect();
+        console.log(error)
+
+        return res.status(500).json({message: 'Algo salio mal, revisar consola del servidor'})
+    }
+
+} 
