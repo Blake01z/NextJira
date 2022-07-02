665bc343f4e64b307440a5aa5134c9f520c7f788
Daniel Correa <daniel.correa92@hotmail.com>
2022-07-02 18:47:23 -0500

    Cambios en el entry provider

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -15,7 +15,7 @@ const Entries_INITIAL_STATE: EntriesState = {
     entries: []
 }
 
-export const EntriesProvider:FC = ({children}) => {
+export const EntriesProvider:FC = ({children}:any) => {
 
     const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
     const {enqueueSnackbar} = useSnackbar();
