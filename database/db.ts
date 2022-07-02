8c1d33ef427f4f4442075fa90b1fec274470a292
Daniel Correa <daniel.correa92@hotmail.com>
2022-07-02 17:54:36 -0500

    Ajustes a mongo db

--- database/db.ts
+++ database/db.ts
@@ -7,21 +7,21 @@ import mongoose, { mongo } from "mongoose";
     * 3 = diconnecting
 */
 
-const mongooConnection = {
+const mongoConnection = {
     isConnected: 0
 }
 
 export const connect = async () => {
     
-    if(mongooConnection.isConnected){
+    if(mongoConnection.isConnected){
         console.log('Ya estabamos conectados');
         return;
     }
 
     if(mongoose.connections.length > 0){
-        mongooConnection.isConnected = mongoose.connections[0].readyState;
+        mongoConnection.isConnected = mongoose.connections[0].readyState;
 
-        if(mongooConnection.isConnected === 1){
+        if(mongoConnection.isConnected === 1){
             console.log('Usando Conexion anterior');
             return;
         }
@@ -29,7 +29,7 @@ export const connect = async () => {
     }
 
     await mongoose.connect(process.env.MONGO_URL || '');
-    mongooConnection.isConnected = 1;
+    mongoConnection.isConnected = 1;
     console.log('Conectado a MongoDb:', process.env.MONGO_URL);
 }
 
@@ -37,8 +37,10 @@ export const disconnect = async() => {
 
     if(process.env.NODE_ENV === 'development') return;
 
-    if(mongooConnection.isConnected === 0) return;
+    if(mongoConnection.isConnected === 0) return;
 
     await mongoose.disconnect();
+    mongoConnection.isConnected = 0;
+    
     console.log('Desconectado de MongoDB')
 }
